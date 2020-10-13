2c362136d954a9cb096a340307635980a3dc6d10
Pedro Van Thienen <pedrovanthienen@gmail.com>
2020-10-13 17:02:23 -0400

    I believe I met MVP, im gonna keep working for now

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Form";
+
+const initialFormValues = {
+  name: "",
+  email: "",
+
+  role: "",
+};
+function App() {
+  // const [members, setMembers] = useState();
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue,
+    });
+  };
+
+  const submitForm = () => {
+    let newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+    if (!newMember.username || !newMember.email || !newMember.role) return;
+  };
+
+  return (
+    <div className="App">
+      <h1>Working</h1>
+      <Form values={formValues} update={updateForm} submit={submitForm} />
+    </div>
+  );
+}
+
+export default App;
